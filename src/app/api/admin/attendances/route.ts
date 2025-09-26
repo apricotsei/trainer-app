e76c6ae2da4ae7fdb553433f88fbc13eb96bb279
sei <yyy.sei@icloud.com>
2025-09-26 15:39:37 +0900

    fix: authOptions import

--- src/app/api/admin/attendances/route.ts
+++ src/app/api/admin/attendances/route.ts
@@ -1,6 +1,6 @@
 import { db } from '@/lib/db';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { NextRequest, NextResponse } from 'next/server';
 import type { RowDataPacket } from 'mysql2';
 
